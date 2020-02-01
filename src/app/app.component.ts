a900628492351c06282063c9ae795793bca50da9
Mario Souto <soutomarios@gmail.com>
2020-02-01 16:24:03 -0300

    commit do barulho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmail';
+  title = 'Caixa de entrada';
+  isNewEmailFormActive = false;
+
+  toggleNewEmailFormActive() {
+    this.isNewEmailFormActive = !this.isNewEmailFormActive;
+  }
 }
